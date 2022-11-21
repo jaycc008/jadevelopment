f3aa36fb394bad0d2cf7064b7296e5666be57c20
Jayce Ardon <jayce.ardon@gmail.com>
2022-11-21 15:05:10 -0600

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+	plugins: [
+		purgecss({
+			content: ['./**/*.html'],
+		}),
+		{
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	],
 }
