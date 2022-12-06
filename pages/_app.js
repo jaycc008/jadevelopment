fa7bfceec15fcc32acc4b78ef030ccc10f380990
Jayce Ardon <jayce.ardon@gmail.com>
2022-12-05 20:07:26 -0600

    add header/footer to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+	return (
+		<div className='px-20'>
+			<Header />
+			<Component {...pageProps} />
+			<Footer />
+		</div>
+	)
 }
 
 export default MyApp
