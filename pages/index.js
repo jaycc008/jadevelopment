d4bccce353355c35e5ea3f9784836d8d00dc8675
Jayce Ardon <jayce.ardon@gmail.com>
2023-02-27 09:15:45 -0600

    content on lg screens

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,188 @@
-const Home = () => <main>Home</main>
+import Image from 'next/image'
+import profilePic from '../public/jayce.svg'
+import arrowRight from '../public/arrow-right.svg'
+import design from '../public/design.svg'
+import develop from '../public/develop.svg'
+import deploy from '../public/deploy.svg'
+import robStuurman from '../public/rob-stuurman.png'
+import sorryThieves from '../public/sorry-thieves.png'
+import vue from '../public/vue.svg'
+import nuxt from '../public/nuxt.svg'
+import vuetify from '../public/vuetify.svg'
+import react from '../public/react.svg'
+import nextjs from '../public/nextjs.svg'
+import reactSpring from '../public/react-spring.svg'
+import Link from 'next/link'
+
+const Home = () => {
+	return (
+		<div className='container'>
+			<section className='pb-32'>
+				<h1 className='text-[80px] font-sans font-bold'>Creative Developer</h1>
+				<div className='grid grid-cols-12 gap-5 mt-18'>
+					<h2 className='text-[2.5rem] mt-8 font-sans font-bold col-span-6 col-end-7'>
+						I design logo&apos;s and websites with{' '}
+						<span className='text-pink-1'>Illustrator</span> and{' '}
+						<span className='text-pink-3'>Figma</span>
+					</h2>
+					<h2 className='text-[2.5rem] mt-8 font-sans font-bold col-span-6 col-end-7'>
+						I build webapplications and websites with{' '}
+						<span className='text-blue'>&quot;TECH STACK&quot;</span>
+					</h2>
+				</div>
+			</section>
+			<section className='pt-8 pb-40'>
+				<h2 className='font-mono'>01. about me</h2>
+				<div className='grid grid-cols-12 gap-5 mt-8'>
+					<Image
+						src={profilePic}
+						alt='Profile picture of Jayce'
+						className='col-span-4 w-full'
+					/>
+					<article className='col-span-7 col-start-6 mt-[22.5rem]'>
+						<p className='font-sans text-[2rem] mb-8'>
+							Hi, my name is{' '}
+							<span className='text-pink-1 font-bold'>Jayce Ardon</span> and
+							I&apos;m a passionate webdeveloper with years of experience in
+							designing, building and testing products. I love working at the
+							crossroads where creativity and technical issues meet. I create
+							memorable web experiences.
+						</p>
+						<p className='font-sans text-[2rem] mb-12'>
+							With a background in testing, I constantly assure I deliver high
+							quality. Branching out into highly technical topics such as
+							networks and security as well as honing my creativity through
+							multiple projects, I am multidisciplinary and well equipped to
+							deal with anything coming my way.
+						</p>
+						<Link href='/about' className='font-mono font-bold text-2xl'>
+							read more <Image className='inline' alt='' src={arrowRight} />
+						</Link>
+					</article>
+				</div>
+			</section>
+			<section className='pt-8'>
+				<h2 className='font-mono mb-18'>02. my services</h2>
+				<div className='grid grid-cols-12 gap-5'>
+					<div className='col-span-6 col-start-2 '>
+						<h3 className='font-sans font-bold text-[2.5rem] mb-8'>Design</h3>
+						<p className='text-2xl mb-16'>
+							To make sure you get what you want it is important to have a
+							design before diving into programming. As such, I prefer to start
+							from scratch by designing and making sure the designs are to your
+							liking.
+						</p>
+					</div>
+					<Image
+						src={design}
+						alt='design icon'
+						className='col-span-1 col-start-10 mt-11'
+					/>
+					<div className='col-span-6 col-start-2 '>
+						<h3 className='font-sans font-bold text-[2.5rem] mb-8'>
+							Development
+						</h3>
+						<p className='text-2xl mb-16'>
+							Once the design is ready, it&apos;s time to actually build your
+							website and put it live. This is where I use my programming skills
+							as well as my testing skills to create a high quality product.
+						</p>
+					</div>
+					<Image
+						src={develop}
+						alt='develop icon'
+						className='col-span-1 col-start-10 mt-11'
+					/>
+					<div className='col-span-6 col-start-2 '>
+						<h3 className='font-sans font-bold text-[2.5rem] mb-8'>
+							Deployment & Hosting
+						</h3>
+						<p className='text-2xl mb-16'>
+							Your product is ready, now all that is left to do is make sure it
+							is deployed online. This step is needed so you actually get to see
+							your website when you go to your domain. I prefer to work from
+							start to finish and as such prefer to help out with hosting as
+							well.
+						</p>
+					</div>
+					<Image
+						src={deploy}
+						alt='deploy icon'
+						className='col-span-1 col-start-10 mt-11'
+					/>
+					<Link
+						href='/services'
+						className='font-mono font-bold text-2xl col-span-12 col-start-2'
+					>
+						read more <Image className='inline' alt='' src={arrowRight} />
+					</Link>
+				</div>
+			</section>
+			<section className='pt-24'>
+				<h2 className='font-mono mb-18'>03. my work</h2>
+				<div className='grid grid-cols-12 gap-5'>
+					<div className='col-span-4 col-start-2'>
+						<h3 className='font-sans font-bold text-[2.5rem] mt-50 mb-16'>
+							robstuurman.nl
+						</h3>
+						<ul>
+							<li className='flex items-center mb-10'>
+								<Image src={vue} alt='vue icon' className='mr-4' />
+								<span className='text-[1.5rem]'>Vue.js</span>
+							</li>
+							<li className='flex items-center mb-10'>
+								<Image src={nuxt} alt='nuxt.js icon' className='mr-4' />
+								<span className='text-[1.5rem]'>NuxtJS</span>
+							</li>
+							<li className='flex items-center mb-10'>
+								<Image src={vuetify} alt='vuetify icon' className='mr-4' />
+								<span className='text-[1.5rem]'>Vuetify</span>
+							</li>
+						</ul>
+					</div>
+					<Image
+						src={robStuurman}
+						alt='Screenshot of robstuurman.nl website'
+						className='col-span-7 col-start-6 w-full'
+					/>
+					<Image
+						src={sorryThieves}
+						alt="Screenshot of VanMoof's SorryThieves campaign website"
+						className='col-span-7 col-start-1 w-full mt-50'
+					/>
+					<div className='col-span-5 col-start-9 mt-100'>
+						<h3 className='font-sans font-bold text-[2.5rem] mb-16'>
+							VanMoof - SorryThieves
+						</h3>
+						<ul>
+							<li className='flex items-center mb-10'>
+								<Image src={react} alt='react icon' className='mr-4' />
+								<span className='text-[1.5rem]'>React.js</span>
+							</li>
+							<li className='flex items-center mb-10'>
+								<Image src={nextjs} alt='next.js icon' className='mr-4' />
+							</li>
+							<li className='flex items-center mb-10'>
+								<Image
+									src={reactSpring}
+									alt='react spring icon'
+									className='mr-4'
+								/>
+								<span className='text-[1.5rem]'>React Spring</span>
+							</li>
+						</ul>
+					</div>
+					<Link
+						href='/work'
+						className='font-mono font-bold text-2xl col-span-12 col-start-2'
+					>
+						view more <Image className='inline' alt='' src={arrowRight} />
+					</Link>
+				</div>
+			</section>
+			<section className='spacer h-[500px]'></section>
+		</div>
+	)
+}
 
 export default Home
