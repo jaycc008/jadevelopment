e9cdd39b54822dfba15609746541f8bb801b8beb
Jayce Ardon <jayce.ardon@gmail.com>
2023-06-28 14:54:18 -0600

    addition of some fringe cases in components, minor layout changes

--- components/KeyVisual.js
+++ components/KeyVisual.js
@@ -1,16 +1,26 @@
-const KeyVisual = ({ pageName, pageHeader }) => {
+const KeyVisual = ({ pageName, pageHeader, src, children, className }) => {
+	let bgClass
+
+	if (src) {
+		bgClass = `bg-${src}-background`
+	} else {
+		bgClass = `bg-${pageName.toLowerCase().replace(/\s+/g, '')}-background`
+	}
+
 	return (
 		<section
 			className={
-				'h-[80vh] full-width bg-' +
-				pageName.toLowerCase().replace(/\s+/g, '') +
-				'-background bg-cover bg-center lg:bg-right-top mb-20 flex flex-col justify-end'
+				'h-[80vh] full-width bg-cover bg-center lg:bg-right-top flex flex-col justify-end mb-10 md:mb-20 ' +
+				bgClass
 			}
 		>
-			<div className='full-width xl:px-10 lg:px-8 md:px-6 px-4 min-h-1/2 keyvisual-gradient flex flex-col justify-end'>
+			<div className='full-width min-h-1/2 keyvisual-gradient flex flex-col justify-end px-4 md:px-6 lg:px-8 xl:px-10'>
 				<div className='container mx-auto text-white my-12'>
 					<span className='text-xs'>{pageName}</span>
-					<h1 className='text-xl font-serif w-5/12'>{pageHeader}</h1>
+					<h1 className='font-serif text-lg md:text-xl xl:w-5/12'>
+						{pageHeader}
+					</h1>
+					{children}
 				</div>
 			</div>
 		</section>
--- components/PageSection.js
+++ components/PageSection.js
@@ -1,8 +1,8 @@
 import { Cols } from './Cols'
 const PageSection = ({ index, header, largerGapY, children }) => {
 	return (
-		<section className='pb-50'>
-			<h2 className='font-serif mb-18'>
+		<section className={children ? 'pb-20 sm:pb-30 lg:pb-50' : ''}>
+			<h2 className='font-serif mb-8 sm:mb-18'>
 				<span className='text-lightgreen'>{index}</span> - {header}
 			</h2>
 			<Cols largerGapY={largerGapY}>{children}</Cols>
