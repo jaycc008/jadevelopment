df69da7b3bcaadf766082a4f679c8ff6feaa4b74
Jayce Ardon <jayce.ardon@gmail.com>
2023-07-06 12:27:40 -0600

    add Head

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { useEffect } from 'react'
+import Head from 'next/head'
 
 import Interests from '../components/Interests'
 
@@ -11,74 +11,76 @@ import { Cta } from '../components/Cta'
 import { Cols } from '../components/Cols'
 
 const About = () => {
-	useEffect(() => {
-		document.title = 'JA Development | About'
-	}, [])
-
 	return (
-		<main>
-			<KeyVisual pageName='About' pageHeader='Creative Web Developer' />
-			<PageSection index='01' header='Jack-of-all-trades'>
-				<Image
-					src={jack}
-					alt='jack of hearts'
-					className='hidden md:block col-span-3 lg:col-span-2 lg:col-start-2'
+		<>
+			<Head>
+				<title>JA Development | About</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+			</Head>
+			<main>
+				<KeyVisual pageName='About' pageHeader='Creative Web Developer' />
+				<PageSection index='01' header='Jack-of-all-trades'>
+					<Image
+						src={jack}
+						alt='jack of hearts'
+						className='hidden md:block col-span-3 lg:col-span-2 lg:col-start-2'
+					/>
+					<div className='col-span-12 sm:col-span-10 md:col-span-6 lg:col-span-6 sm:col-start-2 md:col-start-5 pt-0 md:pt-30'>
+						<p className='sm:mb-4 md:mb-8'>
+							Starting out as tester, I learned programming and designing once I
+							started my Bachelor&quot;s Degree Mediatechnology at the University
+							of Applied Sciences Rotterdam. In my fourth year I wanted to be able
+							to provide the full package, thus I chose the minor Security and
+							Networking. This resulted in me being a fullstack developer with the
+							know-how of design and deployment.
+						</p>
+						<p className='sm:mb-4 md:mb-8'>
+							After working in FinTech, e-commerce, startups and even the Dutch
+							Ski Association I have seen my fair share of different projects.
+							With my diverse work experience, it was only logical to build my own
+							brand and so JA (which is Dutch for YES) was born. So, if you want
+							me to make your project a big success, look no further:
+						</p>
+					</div>
+					<Cta
+						href='/work'
+						className='col-span-12 sm:col-span-10 sm:col-start-2 md:col-start-5 md:col-span-8'
+						linkText='JA, I want that'
+					/>
+				</PageSection>
+				<PageSection
+					index='02'
+					header='Costa Rica based'
+					className='hidden lg:block'
 				/>
-				<div className='col-span-12 sm:col-span-10 md:col-span-6 lg:col-span-6 sm:col-start-2 md:col-start-5 pt-0 md:pt-30'>
-					<p className='sm:mb-4 md:mb-8'>
-						Starting out as tester, I learned programming and designing once I
-						started my Bachelor&quot;s Degree Mediatechnology at the University
-						of Applied Sciences Rotterdam. In my fourth year I wanted to be able
-						to provide the full package, thus I chose the minor Security and
-						Networking. This resulted in me being a fullstack developer with the
-						know-how of design and deployment.
-					</p>
-					<p className='sm:mb-4 md:mb-8'>
-						After working in FinTech, e-commerce, startups and even the Dutch
-						Ski Association I have seen my fair share of different projects.
-						With my diverse work experience, it was only logical to build my own
-						brand and so JA (which is Dutch for YES) was born. So, if you want
-						me to make your project a big success, look no further:
-					</p>
-				</div>
-				<Cta
-					href='/work'
-					className='col-span-12 sm:col-span-10 sm:col-start-2 md:col-start-5 md:col-span-8'
-					linkText='JA, I want that'
-				/>
-			</PageSection>
-			<PageSection
-				index='02'
-				header='Costa Rica based'
-				className='hidden lg:block'
-			/>
-			<KeyVisual src='monkey' bgPosition='bg-center' className='hidden lg:flex'>
-				<Cols>
-					<p className='text-sm xl:text-md font-serif col-span-5 xl:col-span-4 xl:col-start-2 2xl:col-start-1 2xl:col-span-3'>
+				<KeyVisual src='monkey' bgPosition='bg-center' className='hidden lg:flex'>
+					<Cols>
+						<p className='text-sm xl:text-md font-serif col-span-5 xl:col-span-4 xl:col-start-2 2xl:col-start-1 2xl:col-span-3'>
+							When I say I love adventure, I truly mean that. While my first 31
+							years took place in my home country - The Netherlands - we moved to
+							Costa Rica in the fall of 2022. Do you want to go on an{' '}
+							<span className='text-yellow font-bold'>adventure</span> with me?
+						</p>
+					</Cols>
+				</KeyVisual>
+				<PageSection index='02' header='Costa Rica based' className='lg:hidden'>
+					<Image
+						src={monkey}
+						alt='monkey looking through the trees'
+						className='col-span-12 sm:col-span-8 sm:col-start-3 hidden sm:block'
+					/>
+					<p className='text-sm col-span-12'>
 						When I say I love adventure, I truly mean that. While my first 31
 						years took place in my home country - The Netherlands - we moved to
 						Costa Rica in the fall of 2022. Do you want to go on an{' '}
 						<span className='text-yellow font-bold'>adventure</span> with me?
 					</p>
-				</Cols>
-			</KeyVisual>
-			<PageSection index='02' header='Costa Rica based' className='lg:hidden'>
-				<Image
-					src={monkey}
-					alt='monkey looking through the trees'
-					className='col-span-12 sm:col-span-8 sm:col-start-3 hidden sm:block'
-				/>
-				<p className='text-sm col-span-12'>
-					When I say I love adventure, I truly mean that. While my first 31
-					years took place in my home country - The Netherlands - we moved to
-					Costa Rica in the fall of 2022. Do you want to go on an{' '}
-					<span className='text-yellow font-bold'>adventure</span> with me?
-				</p>
-			</PageSection>
-			<PageSection index='03' header='Personal interests'>
-				<Interests />
-			</PageSection>
-		</main>
+				</PageSection>
+				<PageSection index='03' header='Personal interests'>
+					<Interests />
+				</PageSection>
+			</main>
+		</>
 	)
 }
 
--- pages/contact.js
+++ pages/contact.js
@@ -1,49 +1,52 @@
-import { useEffect } from 'react'
+import Head from 'next/head'
+
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { isDesktop } from 'react-device-detect'
 
 const Contact = () => {
-	useEffect(() => {
-		document.title = 'JA Development | Contact'
-	}, [])
-
 	return (
-		<section className='flex h-[100vh]'>
-			<div className='flex-column justify-center m-auto'>
-				<p className='text-sm sm:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-center'>
-					Send me a message!
-				</p>
-				<div className='text-center underline-yellow xl:mt-6 2xl:mt-24'>
-					<a
-						className='text-sm sm:text-md lg:text-lg xl:text-xl 2xl:text-2xl font-serif copy'
-						onClick={e => {
-							const classes = e.target.classList
-							if (!classes.contains('copied')) classes.add('copied')
-							navigator.clipboard.writeText('jayce.ardon@gmail.com')
-							if (!isDesktop) {
-								toast('copied', {
-									toastId: 0,
-									autoClose: true,
-								})
-							}
-						}}
-						onMouseEnter={e => {
-							const classes = e.target.classList
-							if (classes.contains('copied')) classes.remove('copied')
-						}}
-					>
-						jayce.ardon@gmail.com
-					</a>
+		<>
+			<Head>
+				<title>JA Development | Contact</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+			</Head>
+			<section className='flex h-[100vh]'>
+				<div className='flex-column justify-center m-auto'>
+					<p className='text-sm sm:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-center'>
+						Send me a message!
+					</p>
+					<div className='text-center underline-yellow xl:mt-6 2xl:mt-24'>
+						<a
+							className='text-sm sm:text-md lg:text-lg xl:text-xl 2xl:text-2xl font-serif copy'
+							onClick={e => {
+								const classes = e.target.classList
+								if (!classes.contains('copied')) classes.add('copied')
+								navigator.clipboard.writeText('jayce.ardon@gmail.com')
+								if (!isDesktop) {
+									toast('copied', {
+										toastId: 0,
+										autoClose: true,
+									})
+								}
+							}}
+							onMouseEnter={e => {
+								const classes = e.target.classList
+								if (classes.contains('copied')) classes.remove('copied')
+							}}
+						>
+							jayce.ardon@gmail.com
+						</a>
+					</div>
 				</div>
-			</div>
-			<ToastContainer
-				position='bottom-center'
-				toastClassName={() => 'bg-green text-platinum font-serif text-center'}
-				closeButton={false}
-				hideProgressBar={true}
-			/>
-		</section>
+				<ToastContainer
+					position='bottom-center'
+					toastClassName={() => 'bg-green text-platinum font-serif text-center'}
+					closeButton={false}
+					hideProgressBar={true}
+				/>
+			</section>
+		</>
 	)
 }
 
